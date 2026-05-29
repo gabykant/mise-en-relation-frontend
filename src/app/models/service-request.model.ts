eca9fc1b0af389aab5b444bb325dcece329302d9
Kwaye Kant <gabrielkwaye@gmail.com>
2026-05-29 13:42:45 +0100

    Display orders

--- src/app/models/service-request.model.ts
+++ src/app/models/service-request.model.ts
@@ -7,14 +7,12 @@ export enum RequestStatus {
 
 export interface ServiceRequest {
   id?: string;
-  clientId: string;
   clientPhone: string;
-  serviceType: string;
   serviceName: string;
-  location: string;
   zoneName: string;
   description: string;
   status: RequestStatus;
   createdAt: string;
-  artisanName?: string;
+  artisanName: string;
+  updatedAt: string;
 }
