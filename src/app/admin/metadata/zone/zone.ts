78b395a693c3b7416fffa1be8abee060d897e30f
Kwaye Kant <gabrielkwaye@gmail.com>
2026-06-15 01:07:54 +0100

    Add Menu Zones

---
+++ src/app/admin/metadata/zone/zone.ts
@@ -0,0 +1,56 @@
+import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
+import { ArtisanService } from '@services/artisan';
+import { Router } from 'node_modules/@angular/router/types/_router_module-chunk';
+import { FilterPipe } from 'src/app/pipes/filter-pipe';
+import { FormsModule, FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-zone',
+  imports: [CommonModule, RouterModule, FormsModule, FilterPipe],
+  templateUrl: './zone.html',
+  styleUrl: './zone.css',
+})
+export class Zone implements OnInit {
+
+  zones: any[] = [];
+  loading = true;
+
+  constructor(
+    @Inject(ArtisanService) private artisanService: ArtisanService, 
+    private router: Router, 
+    private fb: FormBuilder,
+    private cdr: ChangeDetectorRef) {
+      
+  }
+
+  ngOnInit(): void {
+    this.artisanService.getZones().subscribe({
+      next: (data) => {
+        this.zones = data;
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.log(err);
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+    });
+  }
+
+  goToCreate() {
+    this.router.navigate(['/admin/areas']);
+  }
+
+  // editZone(zone: any) {
+  //   this.router.navigate(['/admin/areas/edit', zone.id]);
+  // }
+
+  // viewZone(zone: any) {
+  //   this.router.navigate(['/admin/areas/view', zone.id]);
+  // }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ArtisanList } from './admin/components/artisan-list/artisan-list';
 import { ArtisanCreate } from './admin/components/artisan-create/artisan-create';
 import { ArtisanEdit } from './components/artisan-edit/artisan-edit';
 import { ArtisanView } from './admin/components/artisan-view/artisan-view';
+import { Zone } from './admin/metadata/zone/zone';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
@@ -17,6 +18,7 @@ export const routes: Routes = [
         children: [
             { path: 'requests', component: ServiceRequestList },
             { path: 'artisans', component: ArtisanList },
+            { path: 'areas', component: Zone },
             { path: 'artisans/create', component: ArtisanCreate },
             { path: 'artisans/edit/:id', component: ArtisanEdit },
             { path: 'artisans/view/:id', component: ArtisanView },
